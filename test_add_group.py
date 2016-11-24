7f20f7f8340a8cd3d8b7564c059d74815e8b086f
ble669 <miroslaw.sommer@wp.pl>
2016-11-24 14:28:24 +0100

    PyCarm

--- test_add_group.py
+++ test_add_group.py
@@ -20,19 +20,14 @@ class test_add_group(unittest.TestCase):
     def test_add_group(self):
 
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="sdfgfsd", header="sdfbgh", footer="sfhr"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
-
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
@@ -44,6 +39,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group ):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group firm
@@ -65,6 +61,7 @@ class test_add_group(unittest.TestCase):
 
     def login(self, wd, username, password):
         # login
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
