9258086c21d0a76185c87edd857b38787953e802
ble669 <miroslaw.sommer@wp.pl>
2016-11-28 21:29:04 +0100

    fixture contact

--- contact.py
+++ contact.py
@@ -1,5 +1,6 @@
 class Contact:
-    def __init__(self, firstname, lastname):
+    def __init__(self, firstname, lastname, middlename):
         self.firstname = firstname
+        self.middlename = middlename
         self.lastname = lastname
 
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,58 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
+import pytest
 from contact import Contact
-import  unittest
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_add_contact(self):
-        success = True
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        self.Login(wd, username="admin", password="secret")
-        self.add_new_contact(wd, Contact(firstname="asdf", lastname="sdfsf"))
-        self.logout(success, wd)
-
-    def logout(self, success, wd):
-        # logout
-        wd.find_element_by_link_text("Wyloguj się").click()
-        self.assertTrue(success)
-
-    def add_new_contact(self, wd, contact):
-        # add_new_contact
-        wd.find_element_by_link_text("nowy wpis").click()
-        #fill contact from
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def Login(self, wd, username, password):
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+from application_for_contact import Application_for_contact
 
 
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application_for_contact()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_contact(app):
+    app.login()
+    app.contact_creation(Contact( firstname="afadf", middlename="sfgsgf", lastname="sgfsgfdg"))
+    app.logout()
+
+
+
+
