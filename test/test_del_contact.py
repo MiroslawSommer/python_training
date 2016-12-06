41fe30da8198c982ddbc36d7f91170df8ff96086
ble669 <miroslaw.sommer@wp.pl>
2016-12-06 17:46:33 +0100

    homework 9

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,3 +1,7 @@
+from model.contact import Contact
+
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.contact_creation(Contact(firstname="test"))
     app.contact.delete_first_contact()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,4 +2,6 @@ from model.contact import Contact
 
 
 def test_modify_contact_name(app):
+    if app.contact.count() == 0:
+        app.contact.contact_creation(Contact(firstname="test"))
     app.contact.modify_contact_name(Contact(firstname="New name"))
