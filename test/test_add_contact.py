df091fcc5fc53abd1fd3c7095df3f033e9d449fe
ble669 <miroslaw.sommer@wp.pl>
2016-12-16 15:54:04 +0100

    homework 12

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,22 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
-from sys import maxsize
 
 
 def test_add_empty_contact(app):
     old_contacts = app.contact.get_contact_list()
     contact = Contact(firstname="a", middlename="b", lastname="c")
     app.contact.contact_creation(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
-    def id_or_max(ct):
-        if ct.id:
-            return int(ct.id)
-        else:
-            return maxsize
-    assert sorted(old_contacts, key=id_or_max) == sorted(new_contacts, key=id_or_max)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
