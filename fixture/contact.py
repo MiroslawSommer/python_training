b1ee5ed697d76bb5126e869e70c42b80ba867f74
ble669 <miroslaw.sommer@wp.pl>
2016-12-22 13:50:28 +0100

    test_phones

--- fixture/contact.py
+++ fixture/contact.py
@@ -67,7 +67,7 @@ class ContactHelper:
         self.open_home_page()
         self.select_contact_by_index(index)
         # open modification form
-        wd.find_element_by_xpath("//table[@id ='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
         # fill contact from
         self.fill_contact_form(new_contact_data)
         # submit modification
@@ -89,8 +89,8 @@ class ContactHelper:
             self.contact_cache = []
             for row in wd.find_elements_by_name("entry"):
                 cells = row.find_elements_by_tag_name("td")
-                firstname = cells[1].text
-                lastname = cells[2].text
+                firstname = cells[2].text
+                lastname = cells[1].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 all_phones = cells[5].text
                 self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
@@ -127,7 +127,7 @@ class ContactHelper:
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
         self.open_contact_view_by_index(index)
-        text = wd.find_element_by_id("content").text
+        text = wd.find_element_by_id("container").text
         homephone = re.search("H: (.*)", text).group(1)
         workphone = re.search("W: (.*)", text).group(1)
         mobilephone = re.search("M: (.*)", text).group(1)
