5bd9414cc03aa5eaf5eae48daf3595bd86071a9e
ble669 <miroslaw.sommer@wp.pl>
2017-01-05 12:49:17 +0100

    homework 18

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,23 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [Contact(firstname="", middlename="", lastname="")] + [
-    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 20),
-            lastname=random_string("lastname", 20))
-    for i in range (5)
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_empty_contact(app, contact):
+def test_add_empty_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.contact_creation(contact)
     assert len(old_contacts) + 1 == app.contact.count()
