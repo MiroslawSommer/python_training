d82e9dfc609f2a6f43aa07dcdc70c7be197e660e
ble669 <miroslaw.sommer@wp.pl>
2017-01-30 14:42:05 +0100

    test 3

--- fixture/contact.py
+++ fixture/contact.py
@@ -42,10 +42,23 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
 
+    def select_contact_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
     def delete_contact_by_index(self, index):
         wd = self.app.wd
         self.open_home_page()
         self.select_contact_by_index(index)
+        # submit deletion
+        wd.find_element_by_xpath("//input[@value='Usuń']").click()
+        wd.switch_to_alert().accept()
+        self.contact_cache = None
+
+    def delete_contact_by_id(self, id):
+        wd = self.app.wd
+        self.open_home_page()
+        self.select_contact_by_id(id)
         # submit deletion
         wd.find_element_by_xpath("//input[@value='Usuń']").click()
         wd.switch_to_alert().accept()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,18 +1,18 @@
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_modify_group_name(app):
+def test_modify_group_name(app, db):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
     group = Group(name="New group")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
+    group.id = old_groups[id].id
+    app.group.modify_group_by_id(group.id)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[index] = group
+    old_groups[id] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
